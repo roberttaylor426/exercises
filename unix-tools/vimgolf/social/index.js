119f3799a1905610ff100d1e732bdb17a5886f38
Robert Taylor <robert.taylor426@gmail.com>
2017-06-26 18:33:41 +0100

    Adding handlebars templating

--- unix-tools/vimgolf/social/index.js
+++ unix-tools/vimgolf/social/index.js
@@ -1,7 +1,28 @@
 var express = require('express')
+var fs = require('fs')
+var hbs = require('hbs')
 
 var app = express()
 
 app.use(express.static('content'))
+app.set('view engine', 'hbs')
+
+app.get('/exercises/:exerciseName', function (req, res) {
+	var exerciseName = req.params.exerciseName
+	fs.readFile('exercises/' + exerciseName + '_title', function(err, name) {
+		if (err) throw err
+		fs.readFile('exercises/' + exerciseName + '_start', function(err, start) {
+			if (err) throw err
+			fs.readFile('exercises/' + exerciseName + '_end', function(err, end) {
+				if (err) throw err
+				res.render('index', { 
+							exercise_name: name.toString(),
+							exercise_start: start.toString(),
+							exercise_end: end.toString() 
+				})
+			})
+		})
+	})
+})
 
 app.listen(3000)
